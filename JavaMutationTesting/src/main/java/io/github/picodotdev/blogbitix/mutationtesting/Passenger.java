e08ad2df69d19ea32befccc659159c333f2a180e
jjmp-cesur <67022777+jjmp-cesur@users.noreply.github.com>
2020-06-17 20:21:20 +0200

    Add files via upload

---
+++ JavaMutationTesting/src/main/java/io/github/picodotdev/blogbitix/mutationtesting/Passenger.java
@@ -0,0 +1,19 @@
+package io.github.picodotdev.blogbitix.mutationtesting;
+
+public class Passenger {
+
+    private int age;
+
+    public Passenger(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void consolePrintName(){
+        System.out.println("Juan Jesús Moreno Pérez");
+    }
+}
+
